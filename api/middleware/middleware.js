48306c11cc091d95ad25e581c276fd0d8b8ee1ac
William Lee <leewp8@gmail.com>
2022-04-13 16:01:34 -0400

    wrote logger function

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,11 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} to ${url}`)
+
+  next();
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +21,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
