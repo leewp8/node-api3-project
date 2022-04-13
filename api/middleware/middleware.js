7090dca8566f8caa1aa5d6d97250ef844f73bf9b
William Lee <leewp8@gmail.com>
2022-04-13 17:21:14 -0400

    added get users function

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,29 +1,55 @@
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString()
   const method = req.method
   const url = req.originalUrl
   console.log(`[${timestamp}] ${method} to ${url}`)
-
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-}
+async function validateUserId(req, res, next) {
+  try {
+    const user = await Users.getById(req.params.id)
+		if (!user) {
+      res.status(404).json({ message: 'user not found'})
+    } else {
+      req.user = user
+      next()
+    }
+   } catch(err) {
+			res.status(500).json({
+				message: 'Error retrieving the user',
+			});
+		};
+  }
+
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if (typeof req.body.name != 'string' || req.body.name.trim() == '') {
+		res.status(400).json({ message: 'missing required name field' });
+		return;
+	}
+
+	req.name = { name: req.body.name.trim() };
+	next();
 }
 
+
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if (typeof req.body.text != 'string' || req.body.text.trim() == '') {
+		res.status(400).json({ message: 'missing required text field' });
+		return;
+	}
+
+	req.text = { text: req.body.text.trim() };
+	next();
 }
 
 // do not forget to expose these functions to other modules
 module.exports = {
   logger,
   validateUserId,
-  validateUser,
-  validatePost
+  validatePost,
+  validateUser
 }
